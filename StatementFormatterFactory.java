ad48a80b55ab6d5802f0e6a474ac0de681b8f6cf
rabesocke <christian-adler@web.de>
2013-05-07 11:39:07 +0200

    add document

---
+++ StatementFormatterFactory.java
@@ -0,0 +1,47 @@
+package bavaria.hightech.statement;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Hashtable;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Properties;
+
+public class StatementFormatterFactory {
+
+	private Map<String, IStatementFormatter> formatter;
+	
+	public StatementFormatterFactory(Locale bankLocale){
+		formatter = new Hashtable<String, IStatementFormatter>();
+		
+		Properties probs = new Properties();
+		
+		FileInputStream in;
+		try {
+			in = new FileInputStream("props/knödel.properties");
+			probs.load(in);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String plainText = probs.getProperty("text/plain");
+		String plainHTML = probs.getProperty("text/html");
+		StatementFormatterTextPlain plain = null;
+		StatementFormatterTextHtml htmlFormatter = null;
+		try {
+			plain = (StatementFormatterTextPlain) Class.forName(plainText).getConstructors()[0].newInstance(bankLocale);
+			htmlFormatter = (StatementFormatterTextHtml) Class.forName(plainHTML).getConstructors()[0].newInstance(bankLocale);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("Keine Klasse instanziert");
+		} 
+		
+		formatter.put("text/plain", plain);
+		formatter.put("text/html", htmlFormatter);
+
+
+	}
+	
+	public IStatementFormatter getStatementFormatter(String mimeType){
+		return formatter.get(mimeType);
+	}
+}
